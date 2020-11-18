faa2e16f71ea77a9812c598e4f4f9acbda1fc528
domosunz <60946780+domosunz@users.noreply.github.com>
2020-11-17 19:09:58 -0600

    Changed to Amino Acid; Added DistanceCalculation

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 '''
 A program that checks for validity of sequences, with ambiguity codes, and 
 then aligns sequences and calculates a distance matrix
-
+Last Modified: 11/17/20
 '''
 
 from Bio.Phylo.TreeConstruction import DistanceCalculator
@@ -9,29 +9,66 @@ from Bio.Align.Applications import ClustalwCommandline
 from Bio import AlignIO
 import sys
 
-
 from readfasta import readfasta
 
+'''
+checksequence - a function that checks if a sequence only contains valid amino acids
+@param sequence_input - the sequence which we are performing the check on
+@return True, if the AA sequence is valid, false otherwise
+'''
 def checksequence(sequence_input):
-    # valid DNA nucleotides A, C, G, and T.
-    valid_dna = 'ACGTRYSWKMBDHVN-'
+    # valid amino acids are all these letters http://130.88.97.239/bioactivity/aacodefrm.html
+    validAA = 'GALMFWKQESPVICYHRNDT-'
+    
     #sequence = input('Please enter a sequence: ').upper()
     sequence = sequence_input.upper()
+    
     # will print True if every character in the sequence belongs to valid_dna
-    if not all(i in valid_dna for i in sequence):
+    if not all(i in validAA for i in sequence):
         return True
     else:
         return False
+'''
+align - a function that takes an unaligned FASTA file and runs it through clustal, 
+    creating a aligned clustal file
+@param unalignedFileName - the file name of the unaligned FASTA file of sequences
+'''
+def clustalAlign(unalignedFileName):
+    cmd = ClustalwCommandline("clustalw2", infile=unalignedFileName)
+    print(cmd)
+
+    # this line sends the clustal command to the terminal
+    stdout, stderr = cmd()
+
+'''
+distanceCalculation - a function that uses Biopython's in-built distance calculator, and saves
+    the distance matrix to a txt file
+@param alignment - a clustal file that has all our sequences aligned
+@return the distance matrix that will be used in tree construction
+'''
+def distanceCalculation(alignment):
+    distanceFileName = "Distance.txt"
+
+    # can also use 'blosum62' instead of identity
+    calculator = DistanceCalculator('identity')
+    distanceMatrix = calculator.get_distance(alignment)
+
+    sys.stdout = open(distanceFileName, "w")
+    print(distanceMatrix)
+    sys.stdout.close()
+
+    return distanceMatrix
 
 def main():
-    '''
-    Reads a FASTA file,
-    '''
-    # Ask for a file name and read in the FASTA file of sequences
-    #fileName = input("Please enter a FASTA file name: ")
-    fileName = "sequences.fa"
-    sequences = readfasta(fileName)
+    
+    #just some filler variables for file names
+    unalignedFileName = "sequences_aa.fa"
+    alignedFileName = "sequences_aa.aln"
 
+    #can ask user for input here at final version
+    sequences = readfasta(unalignedFileName)
+
+    #checks the seqeunces for validty with amino acid letters
     max_len = 0;
     for sequence in sequences:
         if len(sequence[1]) > max_len:
@@ -40,24 +77,14 @@ def main():
             print(sequence[0], "is not valid")
         else:
             print(sequence[0], "is valid!")
-    print("sequences are valid!")
 
-    cmd = ClustalwCommandline("clustalw2", infile=fileName)
-    print(cmd)
+    clustalAlign(unalignedFileName)
 
-    # this line is commented as we already have a file
-    #stdout, stderr = cmd()
+    # prints the alignment file to the terminal
+    alignment = AlignIO.read(alignedFileName, "clustal")
+    print(alignment)
 
-    align = AlignIO.read("sequences.aln", "clustal")
-    print(align)
-
-     # can also use 'blosum62' instead of identity
-    calculator = DistanceCalculator('identity')
-    dm = calculator.get_distance(align)
-
-    sys.stdout = open("Distance.txt", "w")
-    print(dm)
-    sys.stdout.close()
+    distanceMatrix = distanceCalculation(alignment)
 
 if __name__ == "__main__":
     main()
